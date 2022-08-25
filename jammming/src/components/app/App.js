8bdb2f01ab216ed0e27a0b8c879cbd1474bf4674
madmutt <3705987+madmutt@users.noreply.github.com>
2022-08-25 12:45:53 +0100

    Create a Method that Saves the Playlist to a User's Account

--- jammming/src/components/app/App.js
+++ jammming/src/components/app/App.js
@@ -54,6 +54,7 @@ class App extends React.Component {
         this.addTrack = this.addTrack.bind(this);
         this.removeTrack = this.removeTrack.bind(this);
         this.updatePlaylistName = this.updatePlaylistName.bind(this);
+        this.savePlayList = this.savePlayList.bind(this);
     }
 
     addTrack(track){
@@ -77,6 +78,10 @@ class App extends React.Component {
         });
     }
 
+    savePlayList(){
+        const trackURIs = this.state.playlistTracks.map(track => track.uri);
+    }
+
     render() {
         return (
             <div>
@@ -85,7 +90,11 @@ class App extends React.Component {
                     <SearchBar />
                     <div class="App-playlist">
                         <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-                        <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
+                        <Playlist playlistName={this.state.playlistName} 
+                                  playlistTracks={this.state.playlistTracks} 
+                                  onRemove={this.removeTrack} 
+                                  onNameChange={this.updatePlaylistName} 
+                                  onSave={this.savePlayList} />
                     </div>
                 </div>
             </div>
--- jammming/src/components/playlist/Playlist.js
+++ jammming/src/components/playlist/Playlist.js
@@ -10,7 +10,6 @@ class Playlist extends React.Component {
     }
 
     handleNameChange(event){
-        console.log(event.target.value);
         this.props.onNameChange(event.target.value);
     }
 
@@ -21,7 +20,7 @@ class Playlist extends React.Component {
                 <TrackList tracks={this.props.playlistTracks} 
                             onRemove={this.props.onRemove} 
                             isRemoval={true} />
-                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+                <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
             </div>
             );
     }
